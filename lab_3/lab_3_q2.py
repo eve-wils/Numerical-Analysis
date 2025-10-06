cf2df5ef499eee5b6af9efd1e4f59e02fabfaefc
Evelyn Wilson <veev1459@gmail.com>
2025-10-06 19:38:17 -0400

    Plotting question 2

--- lab_3/lab_3_q2.py
+++ lab_3/lab_3_q2.py
@@ -25,10 +25,11 @@ def neville(x, y, x0, n):
     return q[n, n]
 
 # Create points at which to evaluate the function
-num_points = 1000
-s = np.linspace(0, 1, order + 1)
+num_points = 500
+s = np.linspace(0, 1000, order + 1)
 print(s)
-s_points = np.linspace(-2, 2, num_points)
+
+s_points = np.linspace(0, 1000, num_points)
 
 # Define an array to store the calculated points in
 x_results = np.zeros(len(s_points))
@@ -37,80 +38,15 @@ y_results = np.zeros(len(s_points))
 # Call neville function for each point
 for i in range(0, len(s_points)):
     x_results[i] = neville(s, x, s_points[i], order)
+    y_results[i] = neville(s, y, s_points[i], order)
 
-
-'''
-plt.plot(x_points, results_arr[n-5,:], label=f'n={n}')
-
-plt.plot(x_points, f(x_points), 'k--', label='f(x)')
+plt.plot(s_points, x_results, label='x(s)')
+plt.plot(s, x, 'ko', label='x data')
 plt.legend()
-plt.title('Neville Interpolation of f(x)')
+plt.title('Interpolation of x')
 plt.show()
-# Evaluate f(x) at the points
-y_points = f(x_points)
-plt.plot(x_points, results_arr[highest_n - 5], label='n=15')
-plt.plot(x_points, f(x_points), 'k--', label='f(x)')
+plt.plot(s_points, y_results, label='y(s)')
+plt.plot(s, y, 'ko', label='y data')
 plt.legend()
-plt.title('Neville interpolation using venly spaced points of f(x)')
+plt.title('Interpolation of y')
 plt.show()
-
-''''''Repeat with Chebyshev points''''''
-# Define an array to store the calculated points in
-ch_results_arr = np.zeros((highest_n - 5 + 1, len(x_points)))
-# Define an array to store the calculated points in
-for n in range(5, highest_n + 1):
-    # Create array of Chebyshev points, scaling by 2 to get in range [-2, 2]
-    x_array = np.cos((2*np.arange(n+1)+1)*np.pi/(2*(n+1))) * 2 
-    y_array = f(x_array)
-
-    # Call neville function for each point
-    for i in range(0, len(x_points)):
-        ch_results_arr[n-5, i] = neville(x_array, y_array, x_points[i], n)
-
-    plt.plot(x_points, ch_results_arr[n-5,:], label=f'n={n}')
-
-plt.plot(x_points, f(x_points), 'k--', label='f(x)')
-plt.legend()
-plt.title('Chebyshev Interpolation of f(x)')
-plt.show()
-
-plt.plot(x_points, ch_results_arr[highest_n - 5], label='n=15')
-plt.plot(x_points, f(x_points), 'k--', label='f(x)')
-plt.legend()
-plt.title('Chebyshev Interpolation of f(x)')
-plt.show()
-
-# Plot error of evenly spaced points
-dx = 4 / num_points
-# evenly spaced
-cumul_error_arr = np.zeros((highest_n -4))
-# chebyshev
-ch_cumul_error_arr = np.zeros((highest_n -4))
-for n in range(5, highest_n + 1):
-    # evenly spaced
-    cumul_error = 0
-    # chebyshev
-    ch_cumul_error = 0
-    for i in range(len(x_points)):
-        # evenly spaced
-        error_slice = abs(results_arr[n-5, i] - y_points[i])
-        rectangle = error_slice*dx
-        cumul_error += rectangle
-        # chebyshev
-        ch_error_slice = abs(ch_results_arr[n-5, i] - y_points[i])
-        ch_rectangle = ch_error_slice*dx
-        ch_cumul_error += ch_rectangle
-    # evenly spaced
-    cumul_error_arr[n-5] = cumul_error
-    # chebyshev
-    ch_cumul_error_arr[n-5] = ch_cumul_error
-
-n_values = np.arange(5, highest_n + 1)
-width = 0.35
-plt.bar(n_values - width/2, cumul_error_arr, width, color = 'r', label='Evenly Spaced Points')
-plt.bar(n_values + width/2, ch_cumul_error_arr, width, color = 'b', label='Chebyshev Optimal Points')
-plt.xlabel('n')
-plt.ylabel('Cumulative error')
-plt.legend()
-plt.title('Cumulative Error of Neville Interpolation of f(x)')
-plt.show()'''
