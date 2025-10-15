f28ad43d518f74ba6ff7d99fe3896a0c62179ac8
eve-wils <evelyn.wilson@okstate.edu>
2025-10-15 07:59:10 -0400

    Start q2 part ii

--- lab_4/lab_4_q2.py
+++ lab_4/lab_4_q2.py
@@ -62,7 +62,7 @@ for input_angle in range(0, 361):
 
 # Separate x, y values for easier plotting
 x_angles = [r[0] for r in results]
-y_theta2 = [r[1] for r in results]
+phi = [r[1] for r in results]
 
 # Compute derivatives
 forward_diff = []
@@ -70,14 +70,14 @@ centered_diff = []
 
 # Forward difference
 for i in range(len(x_angles) - 1):
-    forward_diff.append((y_theta2[i+1] - y_theta2[i]) / 1)  # 1 degree step
+    forward_diff.append((phi[i+1] - phi[i]) / 1)  # 1 degree step
 
 # Centered difference
 for i in range(1, len(x_angles) - 1):
-    centered_diff.append((y_theta2[i+1] - y_theta2[i-1]) / 2)  # 2 degree step
+    centered_diff.append((phi[i+1] - phi[i-1]) / 2)  # 2 degree step
 
 # Plot 1: θ2 vs θ
-plt.plot(x_angles, y_theta2)
+plt.plot(x_angles, phi)
 plt.title('θ2 vs Input Angle')
 plt.xlabel('Input Angle (degrees)')
 plt.ylabel('θ2 (degrees)')
@@ -111,4 +111,68 @@ plt.show()
 
 ######## PART II ########
 
-# 
+# Compute alpha based on phi
+alpha = []
+for x in phi:
+    alpha.append(x + 149)
+# Do Newton's Method to get Beta
+# Link lengths
+r1 = 1.23
+r2 = 1.26
+r3 = 1.82
+r4 = 2.35
+
+# Results storage
+results = []
+
+# Iterate over input angles
+for input_angle in range(0, 361):
+    theta = math.radians(input_angle)
+    
+    # Reset initial guesses for each input angle
+    theta2 = math.radians(45)
+    theta3 = math.radians(360)
+
+    def f1(th2, th3):
+        return (r2 * np.cos(th2)) + (r3*np.cos(th3)) - (r4*np.cos(theta)) - r1
+    
+    def f2(th2, th3):
+        return (r2 * np.sin(th2)) + (r3*np.sin(th3)) - (r4*np.sin(theta))
+    
+    def df1_dth2(th2, th3):
+        return -r2 * np.sin(th2)
+    
+    def df1_dth3(th2, th3):
+        return -r3 * np.sin(th3)
+    
+    def df2_dth2(th2, th3):
+        return r2 * np.cos(th2)
+    
+    def df2_dth3(th2, th3):
+        return r3 * np.cos(th3)
+    
+    # Newton's method
+    for _ in range(100):
+        F = np.array([f1(theta2, theta3), f2(theta2, theta3)])
+        
+        Jac = np.array([
+            [df1_dth2(theta2, theta3), df1_dth3(theta2, theta3)],
+            [df2_dth2(theta2, theta3), df2_dth3(theta2, theta3)]
+        ])
+        
+        delta = np.linalg.solve(Jac, F)
+        
+        theta2 -= delta[0]
+        theta3 -= delta[1]
+        
+        if np.linalg.norm(delta) < 1e-10:
+            break
+    
+    results.append((input_angle, math.degrees(theta2), math.degrees(theta3)))
+
+# Separate x, y values for easier plotting
+x_angles = [r[0] for r in results]
+beta = [r[1] for r in results]
+# Find dBeta/dt
+
+# Find dBeta^2/dt^2
