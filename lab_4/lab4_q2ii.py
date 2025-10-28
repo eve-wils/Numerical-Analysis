08f1d148e18c05c4731e549c009f90aaddc93b95
Evelyn Wilson <veev1459@gmail.com>
2025-10-28 13:08:27 -0400

    Flesh out trapezoidal and romberg

---
+++ lab_4/lab4_q2ii.py
@@ -0,0 +1,9 @@
+# Lab 4 Question 2, part ii
+# Numerical Methods
+# Evelyn Wilson
+# Due Date: October 15, 2025
+
+# Imports
+import numpy as np
+from scipy import special
+import matplotlib.pyplot as plt
--- lab_5/lab5_q1.py
+++ lab_5/lab5_q1.py
@@ -3,6 +3,7 @@
 # Due: October 30, 2025
 
 import math
+import numpy as np
 
 def f(x):
     return x**2 * math.e**(-x)
@@ -11,28 +12,37 @@ def g(x):
     return x**(1/3)
 
 # Trapezoidal rule
-def trap(function, start, end):
-    return None
+def trap(function, a, b, n):
+    h = (b - a)/n
+    x = np.linspace(a, b, n+1)  # Changed to linspace to include endpoint
+    
+    if function == "f":
+        y = f(x)
+    elif function == "g":
+        y = g(x)
+    return (h/2)*(y[0]+2*np.sum(y[1:-1])+y[-1])  # Fixed parentheses and multiplication
 
 # Romberg integration
-def romberg(function, start, end):
-    # Uses the trapezoidal rule function
-    return None
-
-def comp_trap(function, start, end):
+def romberg(function, a, b):
+    tolerance = 1e-9
+    max = 10
+    romberg = np.zeros((max, max))
+    for i in range(max):
+        n = 2**i
+        romberg[i, 0] = trap(function, a, b, n)
+        for j in range(1, i+1):  # Changed to i+1 to include current level
+            romberg[i, j] = (4**j * romberg[i, j-1] - romberg[i-1, j-1]) / (4**j - 1)
+        if i > 0 and abs(romberg[i, i] - romberg[i-1, i-1]) <= tolerance:  # Added i > 0 check
+            print(f"Converged on the {i}th iteration.")
+            return i, romberg[i, i]
     return None
 
 print(romberg("f", 0, 1))
 print(romberg("g", 0, 1))
 print(romberg("f", 1, 2))
 print(romberg("g", 1, 2))
-
-print(trap("f", 0, 1))
-print(trap("g", 0, 1))
-print(trap("f", 1, 2))
-print(trap("g", 1, 2))
-
-print(comp_trap("f", 0, 1))
-print(comp_trap("g", 0, 1))
-print(comp_trap("f", 1, 2))
-print(comp_trap("g", 1, 2))
+n = 3
+print(trap("f", 0, 1, n))
+print(trap("g", 0, 1, n))
+print(trap("f", 1, 2, n))
+print(trap("g", 1, 2, n))
