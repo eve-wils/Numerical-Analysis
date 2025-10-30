df28f33c3d01f0881744f9145f6b8efec0580135
eve-wils <evelyn.wilson@okstate.edu>
2025-10-30 09:54:27 -0400

    Started Euler

--- lab_5/lab5_q3.py
+++ lab_5/lab5_q3.py
@@ -18,21 +18,32 @@ def dy_dt(y, t):
 def y(t):
     return (t**2)*(np.exp(-t**2))
 
-def euler(n):
+def euler(n, h, t):
+    for j in (1, t:
+
     return None
 
-def mod_euler(n):
+def mod_euler(n, h, t):
     return None
 
-def ab_am_2step_pc(n):
+def ab_am_2step_pc(n, h, t):
     return None
 
-def rk4(n):
+def rk4(n, h, t):
     return None
 
 for i in range(3, 100):
-    delta_t = 2**(-i)
-    
+    h = 2**(-i)
+    # Create an array points t from 1 to 2 with delta_t
+    t = np.arange(1, 2 + h, h)
+    # Call each method to solve the ODE
+    euler_solve = euler(i, h, t)
+    mod_euler_solve = mod_euler(i, h, t)
+    ab_am_solve = ab_am_2step_pc(i, h, t)
+    rk4_solve = rk4(i, h, t)
+
+
+
 
 # Plot the solutions vs exact solution
 
